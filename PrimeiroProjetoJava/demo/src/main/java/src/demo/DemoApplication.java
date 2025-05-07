e5da89666cd846a17387e184fb7b9dc0e53a67f0
Jean <jepaalgo00@gmail.com>
2025-05-07 16:25:53 -0400

    2

--- PrimeiroProjetoJava/demo/src/main/java/src/demo/DemoApplication.java
+++ PrimeiroProjetoJava/demo/src/main/java/src/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.src;
+package src.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SrcApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SrcApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
